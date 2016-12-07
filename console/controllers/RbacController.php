dd9ce54bcb06f63df9652780fbff3a1ade51da68
Cuong Nguyen <cuongnguyen@Ambitions-MacBook-Air.local>
2016-12-07 11:59:38 +0700

    master

---
+++ console/controllers/RbacController.php
@@ -0,0 +1,21 @@
+<?php
+namespace console\controllers;
+
+use Yii;
+use yii\console\Controller;
+
+class RbacController extends Controller
+{
+    public function actionInit()
+    {
+        $auth = Yii::$app->authManager;
+
+        // add "admin" role
+        $admin = $auth->createRole('admin');
+        $auth->add($admin);
+
+        // Assign roles to users. 1 and 2 are IDs returned by IdentityInterface::getId()
+        // usually implemented in your User model.
+        $auth->assign($admin, 1);
+    }
+}
